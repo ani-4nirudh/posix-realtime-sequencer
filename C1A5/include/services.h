0cf125d4b932e017280c357923dafe2219b347d6
ani-4nirudh <anirudh.sai8296@gmail.com>
2026-09-01 10:55:00 +0200

    C1A5: Added file info templates

--- C1A5/include/services.h
+++ C1A5/include/services.h
@@ -1,3 +1,22 @@
+/**************************************************************************
+ * Copyright (C) 2026 by Anirudh Singh
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Anirudh Singh is not liable for any misuse of this material.
+ *************************************************************************/
+
+/**
+ * @file app_config.h
+ * @brief
+ *
+ * @description
+ *
+ * @author Anirudh Singh
+ * @date 20th August 2026
+ */
+
 #ifndef SERVICES_H
 #define SERVICES_H
 
